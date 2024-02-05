4c99305570aedb08faa961dc14f80ddc9210ccf2
mehdibil <105067781+mehdibil@users.noreply.github.com>
2024-02-05 21:24:21 +0100

    keymath.c

--- keymath.c
+++ keymath.c
@@ -24,8 +24,11 @@ const char *EC_constant_P = "fffffffffffffffffffffffffffffffffffffffffffffffffff
 const char *EC_constant_Gx = "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798";
 const char *EC_constant_Gy = "483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8";
 
-const char *formats[3] = {"publickey", "rmd160", "address"};
-const char *looks[2] = {"compress", "uncompress"};
+const char *formats[3] = {"publickey","rmd160","address"};
+const char *looks[2] = {"compress","uncompress"};
+
+const char *EC_constant_min_publickey = "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798";
+const char *EC_constant_max_publickey = "0296516a8f65774275278d0d7420a88df0ac44bd64c7bae07c3fe397c5b3300b23";
 
 void set_publickey(char *param, struct Point *publickey);
 void generate_strpublickey(struct Point *publickey, bool compress, char *dst);
@@ -45,29 +48,8 @@ int FLAG_NUMBER = 0;
 
 mpz_t inversemultiplier, number;
 
-// Define the maximum allowed public key as a constant
-const char *MAX_PUBLIC_KEY = "0296516a8f65774275278d0d7420a88df0ac44bd64c7bae07c3fe397c5b3300b23";
-
-void cleanup() {
-    mpz_clear(EC.p);
-    mpz_clear(EC.n);
-    mpz_clear(G.x);
-    mpz_clear(G.y);
-
-    mpz_clear(A.x);
-    mpz_clear(A.y);
-    mpz_clear(B.x);
-    mpz_clear(B.y);
-    mpz_clear(C.x);
-    mpz_clear(C.y);
-
-    mpz_clear(number);
-    mpz_clear(inversemultiplier);
-}
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)  {
     char buffer_input[1024];
-
     mpz_init_set_str(EC.p, EC_constant_P, 16);
     mpz_init_set_str(EC.n, EC_constant_N, 16);
     mpz_init_set_str(G.x, EC_constant_Gx, 16);
@@ -76,38 +58,39 @@ int main(int argc, char **argv) {
 
     mpz_init_set_ui(A.x, 0);
     mpz_init_set_ui(A.y, 0);
+
     mpz_init_set_ui(B.x, 0);
     mpz_init_set_ui(B.y, 0);
+
     mpz_init_set_ui(C.x, 0);
     mpz_init_set_ui(C.y, 0);
 
     mpz_init(number);
     mpz_init(inversemultiplier);
 
-    if (argc < 4) {
+    if(argc < 4)  {
         printf("Missing parameters\n");
-        cleanup();
         exit(0);
     }
 
-    switch (strlen(argv[1])) {
+    switch(strlen(argv[1]))  {
         case 66:
         case 130:
             set_publickey(argv[1], &A);
             break;
         default:
-            printf("Unknown public key length\n");
-            cleanup();
+            printf("Unknown publickey length\n");
             exit(0);
             break;
     }
-
-    switch (strlen(argv[3])) {
+    
+    switch(strlen(argv[3]))  {
         case 66:
-            if (argv[3][0] == '0' && argv[3][1] == 'x') {
+            if(argv[3][0] == '0' && argv[3][1] == 'x')  {
                 mpz_set_str(number, argv[3], 0);
                 FLAG_NUMBER = 1;
-            } else {
+            }
+            else  {
                 set_publickey(argv[3], &B);
                 FLAG_NUMBER = 0;
             }
@@ -121,19 +104,16 @@ int main(int argc, char **argv) {
             FLAG_NUMBER = 1;
             break;
     }
-
     mpz_mod(number, number, EC.n);
-
-    switch (argv[2][0]) {
+    switch(argv[2][0])  {
         case '+':
-            if (FLAG_NUMBER) {
+            if(FLAG_NUMBER)  {
                 Scalar_Multiplication(G, &B, number);
             }
             Point_Addition(&A, &B, &C);
-
             break;
         case '-':
-            if (FLAG_NUMBER) {
+            if(FLAG_NUMBER)  {
                 Scalar_Multiplication(G, &B, number);
             }
             Point_Negation(&B, &C);
@@ -142,76 +122,63 @@ int main(int argc, char **argv) {
             Point_Addition(&A, &B, &C);
             break;
         case '/':
-            if (!FLAG_NUMBER) {
+            if(!FLAG_NUMBER)  {
                 printf("We don't know how to divide 2 public keys, we need a scalar number\n");
-                cleanup();
                 exit(0);
-            } else {
+            }
+            else  {
                 mpz_invert(inversemultiplier, number, EC.n);
                 Scalar_Multiplication_custom(A, &C, inversemultiplier);
             }
             break;
         case 'x':
-            if (!FLAG_NUMBER) {
+            if(!FLAG_NUMBER)  {
                 printf("We don't know how to multiply 2 public keys, we need a scalar number\n");
-                cleanup();
                 exit(0);
-            } else {
+            }
+            else  {
                 Scalar_Multiplication_custom(A, &C, number);
             }
             break;
     }
-
     generate_strpublickey(&C, true, str_publickey);
+    printf("Result: %s\n\n", str_publickey);
+}
 
-    // Check if the generated public key exceeds the maximum allowed value
-    if (mpz_cmp_str(C.x, MAX_PUBLIC_KEY) > 0 || mpz_cmp_str(C.y, MAX_PUBLIC_KEY) > 0) {
-        printf("Error: The calculated public key exceeds the maximum allowed value.\n");
-        cleanup();
+void generate_strpublickey(struct Point *publickey, bool compress, char *dst)  {
+    memset(dst, 0, 131);
+    if(compress)  {
+        if(mpz_tstbit(publickey->y, 0) == 0)  {    // Even
+            gmp_snprintf(dst, 67, "02%0.64Zx", publickey->x);
+        }
+        else  {
+            gmp_snprintf(dst, 67, "03%0.64Zx", publickey->x);
+        }
+    }
+    else  {
+        gmp_snprintf(dst, 131, "04%0.64Zx%0.64Zx", publickey->x, publickey->y);
+    }
+}
+
+void set_publickey(char *param, struct Point *publickey)  {
+    char hexvalue[65];
+    char *dest;
+    int len;
+    len = strlen(param);
+    dest = (char*) calloc(len+1, 1);
+    if(dest == NULL)  {
+        fprintf(stderr, "[E] Error calloc\n");
         exit(0);
     }
-
-    printf("Result: %s\n\n", str_publickey);
-	
-    cleanup();
-    return 0;
-}
-
-
-void generate_strpublickey(struct Point *publickey,bool compress,char *dst)	{
-	memset(dst,0,131);
-	if(compress)	{
-		if(mpz_tstbit(publickey->y, 0) == 0)	{	// Even
-			gmp_snprintf (dst,67,"02%0.64Zx",publickey->x);
-		}
-		else	{
-			gmp_snprintf(dst,67,"03%0.64Zx",publickey->x);
-		}
-	}
-	else	{
-		gmp_snprintf(dst,131,"04%0.64Zx%0.64Zx",publickey->x,publickey->y);
-	}
-}
-
-void set_publickey(char *param,struct Point *publickey)	{
-	char hexvalue[65];
-	char *dest;
-	int len;
-	len = strlen(param);
-	dest = (char*) calloc(len+1,1);
-	if(dest == NULL)	{
-		fprintf(stderr,"[E] Error calloc\n");
-		exit(0);
-	}
-	memset(hexvalue,0,65);
-	memcpy(dest,param,len);
-	trim(dest," \t\n\r");
-	len = strlen(dest);
-	switch(len)	{
-		case 66:
-			mpz_set_str(publickey->x,dest+2,16);
-		break;
-		case 130:
+    memset(hexvalue, 0, 65);
+    memcpy(dest, param, len);
+    trim(dest, " \t\n\r");
+    len = strlen(dest);
+    switch(len)  {
+        case 66:
+            mpz_set_str(publickey->x, dest+2, 16);
+            break;
+        case 130:
 			memcpy(hexvalue,dest+2,64);
 			mpz_set_str(publickey->x,hexvalue,16);
 			memcpy(hexvalue,dest+66,64);
@@ -284,3 +251,4 @@ void Scalar_Multiplication_custom(struct Point P, struct Point *R, mpz_t m)  {
   mpz_clear(Q.y);
 	mpz_clear(T.x);
   mpz_clear(T.y);
+}

e7a7c5da8d41c751bf87af2ce9cca14510b91e3b
mehdibil <105067781+mehdibil@users.noreply.github.com>
2024-02-05 20:00:17 +0100

    keymath.c

--- keymath.c
+++ keymath.c
@@ -34,15 +34,6 @@ const char *EC_constant_Gy = "483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c
 
 mpz_t min_public_key; // Declare the MPZ variable
 const char *min_public_key_x = "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798";
-int main(int argc, char **argv)  {
-    // ...
-
-    // Move the initialization inside the main function
-    mpz_init_set_str(min_public_key, min_public_key_x, 16); // Use the variable in the function call
-
-    // ...
-}
-
 
 
 const char *formats[3] = {"publickey","rmd160","address"};
@@ -162,6 +153,10 @@ int main(int argc, char **argv)  {
 	}
 	generate_strpublickey(&C,true,str_publickey);
 	printf("Result: %s\n\n",str_publickey);	
+// Free allocated memory before exiting
+    mpz_clear(min_public_key);
+
+    return 0;
 }
 
 void generate_strpublickey(struct Point *publickey,bool compress,char *dst)	{

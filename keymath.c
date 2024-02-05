7ea81690ec04fdf63e55c73dc0e7bf62099f1fb0
mehdibil <105067781+mehdibil@users.noreply.github.com>
2024-02-05 21:59:39 +0100

    keymath.c

--- keymath.c
+++ keymath.c
@@ -193,9 +193,9 @@ void set_publickey(char *param, struct Point *publickey)  {
     }
 
    // Check if the public key is within the specified range
-    if (mpz_cmp(publickey->x, EC_constant_min_publickey_mpz) < 0 ||
-        mpz_cmp(publickey->x, EC_constant_max_publickey_mpz) > 0 ||
-        (len == 66 && dest[0] == '0' && dest[1] == '2')) {
+    if ((mpz_cmp(publickey->x, EC_constant_min_publickey_mpz) <= 0 ||
+         mpz_cmp(publickey->x, EC_constant_max_publickey_mpz) >= 0) &&
+        (len != 66 || (len == 66 && dest[0] != '0' && dest[1] != '2'))) {
         fprintf(stderr, "[E] Public key is outside the allowed range\n");
         exit(0);
     }
